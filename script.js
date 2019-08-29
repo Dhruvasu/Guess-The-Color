4edd4ec559f1c082f7948461b20d85f185b83668
Dhruvasu <dhruvasu1@hotmail.com>
2019-08-29 17:42:25 -0400

    add event listeners for easyBtn and hardBtn

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 var colors;
+var squares;
+var colorDisplay;
+var messageDisplay;
+var pickedColor;
 var resetButton = document.getElementById("newColors");
 var easyBtn = document.querySelector("#easyBtn");
 var hardBtn = document.querySelector("#hardBtn");
 var numOfSquares = 6;
 
-colors = generateColors(6);
+easyBtn.addEventListener("click", function() {
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	colors = generateColors(3);
+	var x = document.getElementsByClassName("hide");
+	for (var i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	numOfSquares = 3;
+	init();
+});
 
+hardBtn.addEventListener("click", function() {
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	colors = generateColors(6);
+	var x = document.getElementsByClassName("hide");
+	for (var i = 0; i < x.length; i++) {
+		x[i].style.display = "block";
+	}
+	numOfSquares = 6;
+	init();
+});
+
+colors = generateColors(6);
+init();
 function generateColors(num) {
 	var listOfColors = [];
 	for (var i = 0; i < num; i++) {
