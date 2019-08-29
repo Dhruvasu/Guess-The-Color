6c2fe0dab4035771a4b595a8ad7ad1c3546a0fde
Dhruvasu <dhruvasu1@hotmail.com>
2019-08-29 18:34:17 -0400

    include functions used for resetting colours

--- script.js
+++ script.js
@@ -41,6 +41,22 @@ resetButton.addEventListener("click", function() {
 });
 colorDisplay.textContent = pickedColor;
 
+for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener("click", function() {
+		var clickedColor = this.style.backgroundColor;
+		if (clickedColor === pickedColor) {
+			messageDisplay.textContent = "CORRECT";
+			changeColors(pickedColor);
+			document.querySelector("h1").style.backgroundColor = pickedColor;
+			resetButton.textContent = "Play Again?";	
+		}
+		else {
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "TRY AGAIN";
+		}
+	});
+}
+
 function init() {
 	squares = document.querySelectorAll(".square");
 	colorDisplay = document.getElementById("colorDisplay");
@@ -50,6 +66,16 @@ function init() {
 		squares[i].style.backgroundColor = colors[i];
 	}
 	colorDisplay.textContent = pickedColor;
+}
+
+function changeColors(color) {
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+
+function pickColor(length) {
+	return Math.floor(Math.random() * length);
 }
 
 function generateColors(num) {
